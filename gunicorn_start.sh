6436d11db52502a9f54af9cca9423f57106dc722
mmxmatej <148610953+mmxmatej@users.noreply.github.com>
2024-12-05 15:55:34 +0100

    Update gunicorn_start.sh

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Putanja do aplikacije
-APP_DIR="/path/to/your/app"
+APP_DIR="/home/deployuser/simple-flask-app"  # Putanja do aplikacije
 # Pokretanje aplikacije koristeći Gunicorn
 cd $APP_DIR
 gunicorn --workers 3 --bind 0.0.0.0:8000 app:app
